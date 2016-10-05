618176f2ef03ca7c88bb94405a884ff5e5284e56
radamadah <adam.nogueira.hadar@gmail.com>
2016-10-05 15:33:24 -0400

    coms4167 update hwk codebase

--- COMS4167/homeworks/anh2130/FOSSSim/GravitationalForce.cpp
+++ COMS4167/homeworks/anh2130/FOSSSim/GravitationalForce.cpp
@@ -21,6 +21,12 @@ void GravitationalForce::addEnergyToTotal( const VectorXs& x, const VectorXs& v,
   assert( x.size()%2 == 0 );
   assert( m_particles.first >= 0 );  assert( m_particles.first < x.size()/2 );
   assert( m_particles.second >= 0 ); assert( m_particles.second < x.size()/2 );
+/*
+  scalar r = (x.segment<2>(2*m_particles.second)-x.segment<2>(2*m_particles.first)).norm();
+  scalar m1 = m(2*m_particles.second);
+  scalar m2 = m(2*m_particles.first);
+  E += -m_G*m1*m2/r;
+*/
         E -= m_G*m(2*m_particles.first)*m(2*m_particles.second)/(x.segment<2>(2*m_particles.second) - x.segment<2>(2*m_particles.first)).norm();
 }
 
@@ -32,8 +38,23 @@ void GravitationalForce::addGradEToTotal( const VectorXs& x, const VectorXs& v, 
   assert( x.size()%2 == 0 );
   assert( m_particles.first >= 0 );  assert( m_particles.first < x.size()/2 );
   assert( m_particles.second >= 0 ); assert( m_particles.second < x.size()/2 );
+/*
+  scalar m1 = m(2*m_particles.second);
+  scalar m2 = m(2*m_particles.first);
+
+  Vector2s nhat = x.segment<2>(2*m_particles.second)-x.segment<2>(2*m_particles.first); 
+  scalar r = nhat.norm(); 
+  assert( r != 0.0 ); 
+  nhat /= r; //< TODO: Roll this division into nhat
+  nhat *= m_G*m1*m2/(r*r);
+  
+  gradE.segment<2>(2*m_particles.first)  -= nhat;
+  gradE.segment<2>(2*m_particles.second) += nhat;
+*/
         Vector2s grav_vec = x.segment<2>(2*m_particles.second) - x.segment<2>(2*m_particles.first);
-        Vector2s grav = m_G*m(2*m_particles.first)*m(2*m_particles.second)*grav_vec/pow(grav_vec.norm(),3.0);
+		scalar length = grav_vec.norm();
+		assert(length != 0);
+        Vector2s grav = m_G*m(2*m_particles.first)*m(2*m_particles.second)*grav_vec/pow(length,3.0);
         gradE.segment<2>(2*m_particles.first)  -= grav;
         gradE.segment<2>(2*m_particles.second) += grav;
 }
@@ -45,8 +66,26 @@ void GravitationalForce::addHessXToTotal( const VectorXs& x, const VectorXs& v, 
   assert( x.size() == hessE.rows() );
   assert( x.size() == hessE.cols() );
   assert( x.size()%2 == 0 );
+/*
+  scalar m1 = m(2*m_particles.second);
+  scalar m2 = m(2*m_particles.first);
+  
+  Vector2s nhat = x.segment<2>(2*m_particles.second)-x.segment<2>(2*m_particles.first); 
+  scalar r = nhat.norm(); 
+  assert( r != 0.0 ); 
+  nhat /= r;
+
+  Matrix2s entry = Matrix2s::Identity()-3.0*nhat*nhat.transpose();
+  entry *= m_G*m1*m2/(r*r*r);
+
+  hessE.block<2,2>(2*m_particles.first,2*m_particles.first)   += entry;
+  hessE.block<2,2>(2*m_particles.second,2*m_particles.second) += entry;
+  hessE.block<2,2>(2*m_particles.first,2*m_particles.second)  -= entry;
+  hessE.block<2,2>(2*m_particles.second,2*m_particles.first)  -= entry;
+*/
         Vector2s grav_vec = x.segment<2>(2*m_particles.second) - x.segment<2>(2*m_particles.first);
         scalar length = grav_vec.norm();
+		assert(length != 0);
         Vector2s n_hat = grav_vec/length;
         Matrix2s big_K = (Matrix2s::Identity()-3*n_hat*n_hat.transpose())*m_G*m(2*m_particles.second)*m(2*m_particles.first)/pow(length,3.0);
         hessE.block<2,2>(2*m_particles.first,2*m_particles.first)   += big_K;
--- COMS4167/homeworks/anh2130/FOSSSim/SpringForce.cpp
+++ COMS4167/homeworks/anh2130/FOSSSim/SpringForce.cpp
@@ -24,6 +24,8 @@ void SpringForce::addEnergyToTotal( const VectorXs& x, const VectorXs& v, const 
   assert( x.size()%2 == 0 );
   assert( m_endpoints.first >= 0 );  assert( m_endpoints.first < x.size()/2 );
   assert( m_endpoints.second >= 0 ); assert( m_endpoints.second < x.size()/2 );
+  //scalar l = (x.segment<2>(2*m_endpoints.second)-x.segment<2>(2*m_endpoints.first)).norm();
+  //E += 0.5*m_k*(l-m_l0)*(l-m_l0);
         Vector2s spring_vec = x.segment<2>(2*m_endpoints.second) - x.segment<2>(2*m_endpoints.first);
         E -= pow(spring_vec.norm() - m_l0,2.0)*m_k/2;
 }
@@ -35,8 +37,26 @@ void SpringForce::addGradEToTotal( const VectorXs& x, const VectorXs& v, const V
   assert( x.size()%2 == 0 );
   assert( m_endpoints.first >= 0 );  assert( m_endpoints.first < x.size()/2 );
   assert( m_endpoints.second >= 0 ); assert( m_endpoints.second < x.size()/2 );
+/*
+  // Compute the elastic component
+  Vector2s nhat = x.segment<2>(2*m_endpoints.second)-x.segment<2>(2*m_endpoints.first); 
+  scalar l = nhat.norm(); 
+  assert( l != 0.0 ); 
+  nhat /= l;
+  Vector2s fdamp = nhat;
+  nhat *= m_k*(l-m_l0);
+  gradE.segment<2>(2*m_endpoints.first)  -= nhat;
+  gradE.segment<2>(2*m_endpoints.second) += nhat;
+
+  // Compute the internal damping
+  // Remember we are computing minus the force here
+  fdamp *= m_b*fdamp.dot(v.segment<2>(2*m_endpoints.second)-v.segment<2>(2*m_endpoints.first));
+  gradE.segment<2>(2*m_endpoints.first)  -= fdamp;
+  gradE.segment<2>(2*m_endpoints.second) += fdamp;
+ */
         Vector2s spring_vec = x.segment<2>(2*m_endpoints.second) - x.segment<2>(2*m_endpoints.first);
         scalar length = spring_vec.norm();
+		assert(length != 0);
         Vector2s n_hat = spring_vec/length;
         Vector2s spring_force = n_hat*(
                 // spring stiffness
@@ -57,9 +77,41 @@ void SpringForce::addHessXToTotal( const VectorXs& x, const VectorXs& v, const V
   assert( x.size()%2 == 0 );
   assert( m_endpoints.first >= 0 );  assert( m_endpoints.first < x.size()/2 );
   assert( m_endpoints.second >= 0 ); assert( m_endpoints.second < x.size()/2 );
+/*
+  // Contribution from elastic component
+  Vector2s nhat = x.segment<2>(2*m_endpoints.second)-x.segment<2>(2*m_endpoints.first); 
+  scalar l = nhat.norm();
+  assert( l != 0 );
+  nhat /= l;
+
+  Matrix2s hess;
+  hess = nhat*nhat.transpose();
+  hess += (l-m_l0)*(Matrix2s::Identity()-hess)/l;
+  hess *= m_k;
+  
+  hessE.block<2,2>(2*m_endpoints.first,2*m_endpoints.first)   += hess;
+  hessE.block<2,2>(2*m_endpoints.second,2*m_endpoints.second) += hess;
+
+  hessE.block<2,2>(2*m_endpoints.first,2*m_endpoints.second) -= hess;
+  hessE.block<2,2>(2*m_endpoints.second,2*m_endpoints.first) -= hess;
+
+  // Contribution from damping
+  Vector2s dv = v.segment<2>(2*m_endpoints.second)-v.segment<2>(2*m_endpoints.first);
+  hess = nhat*dv.transpose();
+  hess.diagonal().array() += nhat.dot(dv);
+  hess -= hess*(nhat*nhat.transpose());
+  hess *= -m_b/l;
+
+  hessE.block<2,2>(2*m_endpoints.first,2*m_endpoints.first)   -= hess;
+  hessE.block<2,2>(2*m_endpoints.second,2*m_endpoints.second) -= hess;
+
+  hessE.block<2,2>(2*m_endpoints.first,2*m_endpoints.second) += hess;
+  hessE.block<2,2>(2*m_endpoints.second,2*m_endpoints.first) += hess;
+*/
         Vector2s spring_vec = x.segment<2>(2*m_endpoints.second) - x.segment<2>(2*m_endpoints.first);
         Vector2s spring_vel = v.segment<2>(2*m_endpoints.second) - v.segment<2>(2*m_endpoints.first); 
         scalar length = spring_vec.norm();
+		assert(length !- 0);
         Vector2s n_hat = spring_vec/length;
         Matrix2s id = Matrix2s::Identity();
         Matrix2s nnT = n_hat*n_hat.transpose();
@@ -83,8 +135,26 @@ void SpringForce::addHessVToTotal( const VectorXs& x, const VectorXs& v, const V
   assert( x.size()%2 == 0 );
   assert( m_endpoints.first >= 0 );  assert( m_endpoints.first < x.size()/2 );
   assert( m_endpoints.second >= 0 ); assert( m_endpoints.second < x.size()/2 );
+/*
+  // Contribution from damping
+  Vector2s nhat = x.segment<2>(2*m_endpoints.second)-x.segment<2>(2*m_endpoints.first); 
+  scalar l = nhat.norm();
+  assert( l != 0 );
+  nhat /= l;
+  
+  Matrix2s hess;
+  hess = m_b*nhat*nhat.transpose();
+  
+  hessE.block<2,2>(2*m_endpoints.first,2*m_endpoints.first)   += hess;
+  hessE.block<2,2>(2*m_endpoints.second,2*m_endpoints.second) += hess;
+  
+  hessE.block<2,2>(2*m_endpoints.first,2*m_endpoints.second) -= hess;
+  hessE.block<2,2>(2*m_endpoints.second,2*m_endpoints.first) -= hess;  
+*/
         Vector2s spring_vec = x.segment<2>(2*m_endpoints.second) - x.segment<2>(2*m_endpoints.first);
-        Vector2s n_hat = spring_vec/spring_vec.norm();
+		scalar length = spring_vec.norm();
+		assert(length != 0);
+        Vector2s n_hat = spring_vec/length;
         Matrix2s big_B = m_b*n_hat*n_hat.transpose();
         hessE.block<2,2>(2*m_endpoints.first,2*m_endpoints.first)   += big_B;
         hessE.block<2,2>(2*m_endpoints.second,2*m_endpoints.second) += big_B;
